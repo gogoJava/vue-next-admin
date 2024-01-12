98b3a35b641c79f5fcaf5e404f16c6efea73ade0
liuxingui <ascarecrow@126.com>
2024-01-12 22:49:55 +0800

    更新

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -21,20 +21,21 @@
 					<div class="login-right-warp-main-title">{{ getThemeConfig.globalTitle }} 欢迎您！</div>
 					<div class="login-right-warp-main-form">
 						<div v-if="!state.isScan">
-							<el-tabs v-model="state.tabsActiveName">
+							<Account />
+							<!-- <el-tabs v-model="state.tabsActiveName">
 								<el-tab-pane :label="$t('message.label.one1')" name="account">
 									<Account />
 								</el-tab-pane>
 								<el-tab-pane :label="$t('message.label.two2')" name="mobile">
 									<Mobile />
 								</el-tab-pane>
-							</el-tabs>
+							</el-tabs> -->
 						</div>
 						<Scan v-if="state.isScan" />
-						<div class="login-content-main-sacn" @click="state.isScan = !state.isScan">
+						<!-- <div class="login-content-main-sacn" @click="state.isScan = !state.isScan">
 							<i class="iconfont" :class="state.isScan ? 'icon-diannao1' : 'icon-barcode-qr'"></i>
 							<div class="login-content-main-sacn-delta"></div>
-						</div>
+						</div> -->
 					</div>
 				</div>
 			</div>
